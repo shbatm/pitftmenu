09f2eb98615b5cef5d1f257c4149a9cd6459a8be
Re4son <Re4son@users.noreply.github.com>
2016-06-13 14:35:11 +1000

    Add Kali-Pi logo

--- pulledpork.sh
+++ pulledpork.sh
@@ -2,5 +2,5 @@
 script="/home/pi/pitftmenu/menu_pause.py"
 setterm -term linux -back default -fore white -clear all
 /usr/bin/env perl /usr/local/bin/pulledpork.pl -c /usr/local/etc/snort/pulledpork.conf -lT
-setterm -term linux -back default -fore black -clear all
 /usr/bin/env python $script
+setterm -term linux -back default -fore black -clear all
